1bb4389bf21a5a25b2e85bb39a1d763e0d94f806
Mathéo Prouchandy <matheo.prouchandy@etu.cyu.fr>
2025-04-15 17:06:07 +0200

    Update fonction.c

--- fonction.c
+++ fonction.c
@@ -101,3 +101,34 @@ void initialisation_champion(FILE *fichier, Champion *champion) {
     strcpy(champion->effet_spe, effet_spe);
 }
 
+void vitesse(Equipe *equipe1, Equipe *equipe2){ //tri les champion par leur vitesse
+    Champion *ordreAttaque[6];
+    Champion min;
+    ordreAttaque[0]=equipe1->perso1;
+    ordreAttaque[1]=equipe1->perso2;
+    ordreAttaque[2]=equipe1->perso3;
+    ordreAttaque[3]=equipe2->perso1;
+    ordreAttaque[4]=equipe2->perso2;
+    ordreAttaque[5]=equipe2->perso3;
+    for(int i=0;i<6;i++){
+        for(int j=i;j<6;j++){
+            if (ordreAttaque[i]->stat->vitesse > ordreAttaque[j]->stat->vitesse){
+                min=ordreAttaque[i];
+                ordreAttaque[i]=ordreAttaque[j];
+                ordreAttaque[j]=min;
+            }
+        }
+    } 
+}
+float degat(Equipe equipe1, Equipe equipe2){//calcule les degat
+
+
+
+}
+void attaque (Equipe *equipe1, Equipe *equipe2){//actualiser la vie des personnage apres une attaque classique
+
+
+
+
+
+}
