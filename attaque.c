3f3d453252b73e69b70bb6388a90679820492f5f
Mathéo Prouchandy <matheo.prouchandy@etu.cyu.fr>
2025-04-18 17:16:44 +0200

    Update attaque.c

--- attaque.c
+++ attaque.c
@@ -20,14 +20,60 @@ void vitesse(Equipe *equipe1, Equipe *equipe2){ //tri les champion par leur vite
         }
     } 
 }
-float degat(Equipe equipe1, Equipe equipe2){//calcule les degats
-    
-
-
+int nbTank(Champion Equipe[]){
+    int nombre;
+    for(int i=0;i<3;i++){
+        if (Equipe[i].classe==Tank){
+            nombre+=1;
+        }
+    }
+    return nombre;
 }
-void attaque (Equipe *equipe1, Equipe *equipe2){//actualiser la vie des personnage apres une attaque classique
 
+int cibleAttaque(Champion Equipe[]){
+    int cible=0;
+    if (nbTank==0){//si il n'y a pas de tank l'attaque cible l'adversaire le plus lent
+        for(int i=0;i<3;i++){
+            if (Equipe[i].stat.vitesse<Equipe[cible].stat.vitesse){
+                cible=i;
+            }
+        }
+    return cible; 
+    }
+    if (nbTank==1){//si il n'y qu'un seul tank l'attaque cible le tank
+        for(int i=0;i<3;i++){
+            if (Equipe[i].classe==Tank){
+                return i;
+            }
+        } 
+    }
+    if (nbTank>1){//si il y a plusieurs tanks l'attaque cible le tank le plus lent
+        if(Equipe[cible].classe!=Tank){//si le premier champion n'est pas un tank on selectionne le suivant
+            cible=1;
+        }
+        for(int i=0;i<3;i++){
+            if (Equipe[i].classe==Tank && Equipe[i].stat.vitesse<Equipe[cible].stat.vitesse){
+                cible = i;
+            }
+        }
+        return cible;
+    }
+}
 
+float degat(Champion personnage){//calcule les degats
+    return personnage.stat.attaque;
+}
+
+void attaquesimple (Champion personnage, Equipe equipeAdverse[]){//actualiser la vie des personnage apres une attaque classique
+    int precision;
+    int cible=cibleAttaque(equipeAdverse[]);
+    if (precision>equipeAdverse[cible].stat.agilite){//si l'attaque touche actualise les pv en prenant en compte la defense
+        equipeAdverse[cible].stat.pvactuel-=(degat(personnage)/(equipeAdverse[cible].stat.defense)/100);
+    }
+    else{
+        printf("l attaque a ete esquive");
+    }
+}
 
 
 
