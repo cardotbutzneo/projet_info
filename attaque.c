13f213e9d27d36abac2a5892a65b3484451061cd
Néo CARDOT-BUTZ <neo.cardot-butz@etu.cyu.fr>
2025-04-18 18:27:45 +0200

    test

--- attaque.c
+++ attaque.c
@@ -1,6 +1,6 @@
 #include "en-tete.h"
 
-
+/*
 void vitesse(Equipe *equipe1, Equipe *equipe2){ //tri les champion par leur vitesse
     Champion *ordreAttaque[6];
     Champion min;
@@ -25,6 +25,7 @@ float degat(Equipe equipe1, Equipe equipe2){//calcule les degats
 
 
 }
+*/
 void attaque (Equipe *equipe1, Equipe *equipe2){//actualiser la vie des personnage apres une attaque classique
 
 
