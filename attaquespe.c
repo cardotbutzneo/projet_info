865c7b2e53c158c359a077260c7ce3e6bf28dbd2
Mathéo Prouchandy <matheo.prouchandy@etu.cyu.fr>
2025-04-24 17:21:59 +0200

    Update attaquespe.c

--- attaquespe.c
+++ attaquespe.c
@@ -1,8 +1,16 @@
 #include "en-tete.h"
 
 
+
 void attaqueSpecial (Champion personnage, Champion equipeAdverse){
-    switch (affichage_saisie_utilisateur(champion))
+    int numero=0;
+    char tab_nom[Nb_champion] = {"Among us", "Captain america","Donkey Kong", "Dr Tenma", "Gandalf", "Golem de fer", "Invader", "Itachi", "Jack Frost", "Jay", "Johnny Hallyday", "Netero", "Nox", "Picsou", "Pierre Chartier", "Shrek", "Tux","Zelda"};
+    for (int i=0; i<Nb_champion;i++){
+        if (tab[i]==personnage.nom){
+            numero=i+1;
+        }
+    }
+    switch (numero)
     {
     case 1:
         attaquesimple(champion, equipe2.perso);
@@ -16,10 +24,6 @@ void attaqueSpecial (Champion personnage, Champion equipeAdverse){
         break;
     }
 
-
-
-
-
-
-
 }
+
+
