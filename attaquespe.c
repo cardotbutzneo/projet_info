b253b7dc08baaaa4fc2bb73e416187b9d69664fb
Mathéo Prouchandy <matheo.prouchandy@etu.cyu.fr>
2025-04-24 17:07:31 +0200

    Update attaquespe.c

--- attaquespe.c
+++ attaquespe.c
@@ -2,6 +2,19 @@
 
 
 void attaqueSpecial (Champion personnage, Champion equipeAdverse){
+    switch (affichage_saisie_utilisateur(champion))
+    {
+    case 1:
+        attaquesimple(champion, equipe2.perso);
+        break;
+    case 2:
+        printf("fonction pas encore definie\n");
+        break;
+    case 3:
+        printf("fonction pas encore definie\n");
+    default:
+        break;
+    }
 
 
 
