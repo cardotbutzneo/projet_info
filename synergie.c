4d07a80c9e69c805503056be429b0caee89fed0e
Alexandre Godineau <alexandre.godineau@etu.cyu.fr>
2025-04-30 16:12:08 +0200

    Ajout fonction verifie_classes

--- synergie.c
+++ synergie.c
@@ -1,3 +1,6 @@
 #include "en-tete.h"
 #include "synergie.h"
-
+/*
+int verifie_classes(Equipe *equipe, int taille) { //check les classes de chaque personnage de l'équipe choisie puis applique le buff si la condition est validée
+  
+ */ 
