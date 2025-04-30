62ebca86187958e7f54e933992596bd220fbf1c1
Alexandre Godineau <alexandre.godineau@etu.cyu.fr>
2025-04-30 18:26:59 +0200

    Update synergie.c

--- synergie.c
+++ synergie.c
@@ -2,7 +2,17 @@
 #include "synergie.h"
 /*
 int verifie_classes(Equipe *equipe, int taille) { //check les classes de chaque personnage de l'équipe choisie puis applique le buff si la condition est validée
-  for (int i = 1; i < nb_champion_par_equipe; i++) {
-    if (equipe.perso[i].classe 
+  int classe[3];
+  classe[1] = 1, classe[2] = 2; classe[3] = 3;
+    for (int i = 1; i < nb_champion_par_equipe; i++) {
+      if (equipe.perso[i+1].classe == equipe.perso[i].classe) {
+        classe[i+1] == classe[i];
+      }
+    }
+    
 
 */
+
+
+
+
