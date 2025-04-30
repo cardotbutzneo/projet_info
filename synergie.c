107dc4995ec83d15a45213157cef984e074b699e
Alexandre Godineau <alexandre.godineau@etu.cyu.fr>
2025-04-30 18:47:40 +0200

    création fonction verifie_classes (pas fini)

--- synergie.c
+++ synergie.c
@@ -1,14 +1,14 @@
 #include "en-tete.h"
 #include "synergie.h"
 /*
-int verifie_classes(Equipe *equipe, int taille) { //check les classes de chaque personnage de l'équipe choisie puis applique le buff si la condition est validée
-  int classe[3];
-  classe[1] = 1, classe[2] = 2; classe[3] = 3;
-    for (int i = 1; i < nb_champion_par_equipe; i++) {
-      if (equipe.perso[i+1].classe == equipe.perso[i].classe) {
-        classe[i+1] == classe[i];
-      }
-    }
+int verifie_classes(Equipe *equipe) { //check les classes de chaque personnage de l'équipe choisie puis applique le buff si la condition est validée
+  if ( (equipe.perso[1].classe == equipe.perso[2].classe) && (equipe.perso[2].classe == equipe.perso[3].classe) ) {
+    return 0;
+  }
+  if ( (equipe.perso[1].classe != equipe.perso[2].classe) && (equipe.perso[2].classe != equipe.perso[3].classe) ) {
+    return 1;
+  }
+
     
 
 */
