14f5afac447293430f8abe4ab0b6c415a371211e
Mathéo Prouchandy <matheo.prouchandy@etu.cyu.fr>
2025-05-07 15:35:58 +0200

    Update stat.h

--- include/stat.h
+++ include/stat.h
@@ -5,15 +5,6 @@
 
 // Fonctions d'affichage
 int vivant(Champion personnage){
-void afficher_champion_init(Champion *champion_soutien, Champion *champion_tank, Champion *champion_dps, int soutien_count, int tank_count, int dps_count);
-void afficher_classe(Champion *champion);
-void afficher_equipe(Equipe equipe1);
-void afficher_equipes_cote_a_cote(Equipe equipe1, Equipe equipe2);
-void afficher_degat_recu(Champion cible, Champion attaquant, int type_attaque);
-void saisie_utilisateur(Champion *champion, Equipe *equipe2);
-void separation_des_partie();
-void sauter_ligne(FILE *fichier);
-int affichage_saisie_utilisateur(Champion champion);
-void affichage_initial();
-int type_os(int type_OS);
-#endif // AFFICHAGE_H
+void verifstat(Champion personnage){
+void verifstatComplet(Champion equipe[],Champion equipeAdverse[]){
+#endif // STAT_H
