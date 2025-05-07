7a1e5d70bbfd7d7c5790d88127f3babebd2374d2
Mathéo Prouchandy <matheo.prouchandy@etu.cyu.fr>
2025-05-07 14:59:38 +0200

    Update stat.c

--- src/stat.c
+++ src/stat.c
@@ -11,17 +11,17 @@ int vivant(Champion personnage){
 }
 
 void verifstat(Champion personnage){
-  if (personnage.stat.attaque<=0){
+  if (personnage.stat.attaque<=1){
     personnage.stat.attaque=1;
   }
-  if (personnage.stat.defense<0){
-    personnage.stat.defense=0;
+  if (personnage.stat.defense<1){
+    personnage.stat.defense=1;
   }
-  if (personnage.stat.vitesse<0){
-    personnage.stat.vitesse=0;
+  if (personnage.stat.vitesse<1){
+    personnage.stat.vitesse=1;
   }
-  if (personnage.stat.agilite<0){
-    personnage.stat.agilite=0;
+  if (personnage.stat.agilite<1){
+    personnage.stat.agilite=1;
   }
   if (personnage.stat.jauge_actuelle>personnage.stat.jauge_max){
     personnage.stat.jauge_actuelle=personnage.stat.jauge_max;
