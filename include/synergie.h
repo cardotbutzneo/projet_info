ce617090a0c2bf41f6825178094f6ac15eb5544f
Alexandre Godineau <alexandre.godineau@etu.cyu.fr>
2025-05-07 16:10:43 +0200

    Update synergie.h

--- include/synergie.h
+++ include/synergie.h
@@ -1,3 +1,6 @@
+#include "en-tete.h"
+#include "synergie.h"
+
 void buff_troisclassesi_dps(Equipe *equipe);
 void buff_troisclassesi_soutien(Equipe *equipe);
 void buff_troisclassesd(Equipe *equipe);
