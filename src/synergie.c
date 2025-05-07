9c0002d30c5a1969a84602bb72a1d0e060921c05
Alexandre Godineau <alexandre.godineau@etu.cyu.fr>
2025-05-07 15:43:22 +0200

    Update synergie.c

--- src/synergie.c
+++ src/synergie.c
@@ -2,7 +2,7 @@
 #include "synergie.h"
 
 
-void buff_troisclassesi(Equipe *equipe) { //applique le buff 
+void buff_troisclassesi_dps(Equipe *equipe) { //applique le buff 
   //synergie de trois classes identiques
   int i = 0;
   for (i = 0; i < Nb_champion_par_equipe; i++) {
@@ -18,21 +18,28 @@ void buff_troisclassesd(Equipe *equipe) { //applique le buff
   for (int i = 0; i < Nb_champion_par_equipe; i++) {
     equipe->perso[i].stat.attaque += 5;
     equipe->perso[i].stat.defense -= 5;
-    equipe->perso[i].stat.vitesse -= 5;
+    equipe->perso[i].stat.pv_courant -= 5;
     equipe->perso[i].stat.vitesse += 5;
   }
 equipe->synergie += 0.1;
 }
 
 void verifie_classes(Equipe *equipe) { //check les classes de chaque personnage de l'équipe choisie puis lance la fonction dont la condition est validée
-  if ( (equipe->perso[1].classe == equipe->perso[2].classe) && (equipe->perso[2].classe == equipe->perso[3].classe) ) {
-    buff_troisclassesi(equipe);
+  if ( strcmp(equipe->perso[0].classe, equipe->perso[1].classe) == 0 && strcmp(equipe->perso[1].classe, equipe->perso[2].classe) == 0 && strcmp(equipe->perso[0].classe, "dps") == 0 ) {
+    buff_troisclassesi_dps(equipe);
     return;
   }
-  if ( (equipe->perso[1].classe != equipe->perso[2].classe) && (equipe->perso[2].classe != equipe->perso[3].classe) ) {
+  if ( strcmp(equipe->perso[0].classe, equipe->perso[1].classe) != 0 && strcmp(equipe->perso[1].classe, equipe->perso[2].classe) != 0 && strcmp(equipe->perso[0].classe, equipe->perso[2].classe) != 0 ) {
+    buff_troisclassesd(equipe);
     return;
   }
 }
+
+
+
+
+
+
 
 
 
