d6a2a6cb6b9cb1d31112c199b93f1db28e8630fa
Alexandre Godineau <alexandre.godineau@etu.cyu.fr>
2025-05-09 15:16:42 +0200

    Create couleurs.h

---
+++ include/couleurs.h
@@ -0,0 +1,15 @@
+// couleurs.h
+
+#ifndef COULEURS_H
+#define COULEURS_H
+
+// Codes de couleur ANSI
+#define RESET   "\033[0m"
+#define ROUGE   "\033[1;31m"
+#define BLEU    "\033[1;34m"
+#define VERT    "\033[1;32m"
+#define JAUNE   "\033[1;33m"
+#define MAGENTA "\033[1;35m"
+#define CYAN    "\033[1;36m"
+
+#endif // COULEURS_H
