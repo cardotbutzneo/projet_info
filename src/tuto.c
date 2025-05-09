b79be0dc5c26e4e10856e8ab8f3243b10c0a9cd6
Alexandre Godineau <alexandre.godineau@etu.cyu.fr>
2025-05-09 11:44:25 +0200

    Maj affichage du tuto

--- src/tuto.c
+++ src/tuto.c
@@ -3,8 +3,10 @@
 #include "affichage.h"
 #include "fonction.h"
 void afficher_tuto(){
-    printf("Bienvenue \n");
+    //printf("Bienvenue \n");
+    printf("========================================\n");
     printf("Vous jouez a MultiverSus\n");
+    printf("========================================\n\n");
     char rep;
     printf("Connaissez-vous le jeu ? o/n\n");
     int verif = -1;
@@ -18,25 +20,36 @@ void afficher_tuto(){
     }while(verif != 1 || (rep != 'o' && rep != 'n'));
 
     if (rep == 'n'){
-        printf("Vous jouez a un jeu developpe par l'équipe I\n");
-        printf("Le jeu se joue au tour par tour\n");
+        printf("Vous jouez a un jeu developpe par l'equipe I\n\n");
+        printf("========================================\n\n");
+        printf("Instructions du jeu :\n");
+        printf("========================================\n\n");
+        printf("Le jeu se joue en 2 equipes de 3 champions !\n\n");
+        Sleep(time_sleep);
+        printf("Le but du jeu est de choisir une equipe de champions et de les faire combattre contre une autre equipe de champions !\n\n");
+        Sleep(time_sleep);
+        printf("Chaque champion a des statistiques et des attaques speciaux qui lui sont propres !\n\n");
+        Sleep(time_sleep);
+        printf("Il existe trois classes de champions :\n");
+        Sleep(time_sleep);
+        printf("1. Tank : ils ont beaucoup de PV et de defense, mais peu d'attaque\n");
+        Sleep(time_sleep);
+        printf("2. DPS : ils ont beaucoup d'attaque, mais peu de PV et de defense\n");
+        Sleep(time_sleep);
+        printf("3. Soutien : ils ont peu de PV et d'attaque, mais beaucoup de defense\n\n");
+        Sleep(time_sleep);
+        printf("Il existe aussi des synergies entre les champions... Et beaucoup d'autres surprises a decouvrir !\n\n");
+        Sleep(time_sleep);
+        printf("La premiere equipe qui vainc les champions ennemis gagne !\n\n");
+        Sleep(time_sleep);
+        printf("========================================\n\n");
+        Sleep(time_sleep);
         printf("Choisisez vos champions, puis... ");
         Sleep(time_sleep);
-        printf("combattrez !");
-        printf("La première equipe qui vainc les champions ennemis gagne !\n");
+        printf("COMBATTEEEZZZZ !");
+        Sleep(time_sleep);
+        printf("\n\n");
     }
 
 
-}
-
-void note(){
-    printf("Avez-vous aimé le jeu ? (/10)\n");
-    int note;
-    do{
-        scanf("%d",&note);
-        vider_buffer_scanf();
-    }while(note<0 || note > 10);
-    if (note >= 8){
-        printf("Merci beaucoup !\n");
-    }
 }
